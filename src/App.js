2a50b707f185b205a45cc0cde16a6283c7209531
Khoa Tran <jacktran1799@gmaail.com>
2020-02-12 20:50:35 -0600

    finsihed project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from "react-router-dom";
 
 const App = () => (
+
   <div>
+    <Route exact path='/Home' component={Home} />
+    <Route exact path='/About' component={About} />
+    <Route exact path='/Contact' component={Contact} />
     <Navigation />
   </div>
 );
